a33f4bbc8e5a9ca27c7d953458fd4c848c55be9e
David Binder <david.binder@uni-tuebingen.de>
2024-07-25 16:56:31 +0200

    Apply fourmolu

--- src/Test/Mendel/MutationOperator.hs
+++ src/Test/Mendel/MutationOperator.hs
@@ -1,74 +1,87 @@
-{-#  LANGUAGE Rank2Types, FlexibleInstances, ConstraintKinds #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE Rank2Types #-}
+
 -- | Mutation operators
-module Test.Mendel.MutationOperator (MuOp
-          , Mutable(..)
-          , (==>*)
-          , (*==>*)
-          , (~~>)
-          , mkMpMuOp
-          , same
-          , Module_
-          , Exp_
-          , Decl_
-         -- , GuardedRhs_
-         , getSpan
-          ) where
+module Test.Mendel.MutationOperator (
+    MuOp,
+    Mutable (..),
+    (==>*),
+    (*==>*),
+    (~~>),
+    mkMpMuOp,
+    same,
+    Module_,
+    Exp_,
+    Decl_,
+    -- , GuardedRhs_
+    getSpan,
+) where
 
-import qualified Data.Generics as G
 import Control.Monad (MonadPlus, mzero)
-
+import Data.Generics qualified as G
 import GHC.Hs
---import Language.Haskell.Syntax.Expr (GRHS)
---import Data.Data (Data(toConstr))
+-- import Language.Haskell.Syntax.Expr (GRHS)
+-- import Data.Data (Data(toConstr))
+
+import GHC.Types.SrcLoc
 import GHC.Utils.Outputable
 import Language.Haskell.GhclibParserEx.GHC.Hs.ExtendInstances
-import GHC.Types.SrcLoc
 
 type Module_ = HsModule GhcPs
+
 type Exp_ = LHsExpr GhcPs
+
 type Decl_ = LHsDecl GhcPs
--- type GuardedRhs_ = GRHS GhcPs (LocatedA GhcPs) 
+
+-- type GuardedRhs_ = GRHS GhcPs (LocatedA GhcPs)
 
 -- | MuOp constructor used to specify mutation transformation
-data MuOp = E  (Exp_, Exp_)
-          | D  (Decl_, Decl_)
-         -- | G  (GuardedRhs_, GuardedRhs_)
+data MuOp
+    = E (Exp_, Exp_)
+    | D (Decl_, Decl_)
+
+-- \| G  (GuardedRhs_, GuardedRhs_)
 
 instance Eq MuOp where
-  (==) (E (x1, y1)) (E (x2, y2)) = astEq x1 x2 && astEq y1 y2
-  (==) (D (x1, y1)) (D (x2, y2)) = astEq x1 x2 && astEq y1 y2
-  (==) (D _) (E _) = False
-  (==) (E _) (D _) = False
-
+    (==) (E (x1, y1)) (E (x2, y2)) = astEq x1 x2 && astEq y1 y2
+    (==) (D (x1, y1)) (D (x2, y2)) = astEq x1 x2 && astEq y1 y2
+    (==) (D _) (E _) = False
+    (==) (E _) (D _) = False
 
 -- How do I get the Annotated (a SrcSpanInfo) on apply's signature?
+
 -- | getSpan retrieve the span as a tuple
 getSpan :: MuOp -> (Int, Int, Int, Int)
-getSpan m = (startLine, startCol, endLine, endCol) 
-  where startLine = getInt srcSpanStartLine lspan
-        startCol  = getInt srcSpanStartCol lspan
-        endLine   = getInt srcSpanEndLine lspan
-        endCol    = getInt srcSpanEndCol lspan
-        getInt :: (RealSrcSpan -> Int) -> Maybe RealSrcSpan -> Int
-        getInt f (Just x) = f x 
-        getInt _ Nothing = error "No Source Span"
-        getSpan' (E  (a,_)) = getLocA a
-        getSpan' (D  (a,_)) = getLocA a
-        -- getSpan' (G  (a,_)) = ann a
-        lspan = srcSpanToRealSrcSpan $ getSpan' m
+getSpan m = (startLine, startCol, endLine, endCol)
+  where
+    startLine = getInt srcSpanStartLine lspan
+    startCol = getInt srcSpanStartCol lspan
+    endLine = getInt srcSpanEndLine lspan
+    endCol = getInt srcSpanEndCol lspan
+    getInt :: (RealSrcSpan -> Int) -> Maybe RealSrcSpan -> Int
+    getInt f (Just x) = f x
+    getInt _ Nothing = error "No Source Span"
+    getSpan' (E (a, _)) = getLocA a
+    getSpan' (D (a, _)) = getLocA a
+    -- getSpan' (G  (a,_)) = ann a
+    lspan = srcSpanToRealSrcSpan $ getSpan' m
 
--- | The function `same` applies on a `MuOP` determining if transformation is
--- between same values.
+{- | The function `same` applies on a `MuOP` determining if transformation is
+between same values.
+-}
 same :: MuOp -> Bool
-same (E (x,y)) = astEq x y
-same (D (x,y)) = astEq x y
+same (E (x, y)) = astEq x y
+same (D (x, y)) = astEq x y
+
 -- same (G (x,y)) = x == y
 
 -- | A wrapper over mkMp
 mkMpMuOp :: (MonadPlus m, G.Typeable a) => MuOp -> a -> m a
-mkMpMuOp (E (x,y)) = G.mkMp (x ~~> y) 
-mkMpMuOp (D (x,y)) = G.mkMp (x ~~> y)  
--- mkMpMuOp (G (x,y)) = G.mkMp (x ~~> y) 
+mkMpMuOp (E (x, y)) = G.mkMp (x ~~> y)
+mkMpMuOp (D (x, y)) = G.mkMp (x ~~> y)
+
+-- mkMpMuOp (G (x,y)) = G.mkMp (x ~~> y)
 
 -- | Show a specified mutation
 showM :: (Outputable a, Outputable a1) => (a, a1) -> String
@@ -76,38 +89,44 @@ showM (s, t) = "{\n" ++ showPprUnsafe s ++ "\n} ==> {\n" ++ showPprUnsafe t ++ "
 
 -- | MuOp instance for Show
 instance Show MuOp where
-  show (E m) = showM m
-  show (D m) = showM m
-  -- show (G p) = showM p
+    show (E m) = showM m
+    show (D m) = showM m
+
+-- show (G p) = showM p
 
 -- | Mutation operation representing translation from one fn to another fn.
 class Mutable a where
-  (==>) :: a -> a -> MuOp
+    (==>) :: a -> a -> MuOp
 
--- | The function `==>*` pairs up the given element with all elements of the
--- second list, and applies `==>` on them.
-(==>*) :: Mutable a => a -> [a] -> [MuOp]
+{- | The function `==>*` pairs up the given element with all elements of the
+second list, and applies `==>` on them.
+-}
+(==>*) :: (Mutable a) => a -> [a] -> [MuOp]
 (==>*) x = map (x ==>)
 
--- | The function `*==>*` pairs up all elements of first list with all elements
--- of second list and applies `==>` between them.
-(*==>*) :: Mutable a => [a] -> [a] -> [MuOp]
+{- | The function `*==>*` pairs up all elements of first list with all elements
+of second list and applies `==>` between them.
+-}
+(*==>*) :: (Mutable a) => [a] -> [a] -> [MuOp]
 xs *==>* ys = concatMap (==>* ys) xs
 
--- | The function `~~>` accepts two values, and returns a function
--- that if given a value equal to first, returns second
--- we handle x ~~> x separately
+{- | The function `~~>` accepts two values, and returns a function
+that if given a value equal to first, returns second
+we handle x ~~> x separately
+-}
 (~~>) :: (MonadPlus m, G.Data a) => a -> a -> a -> m a
 x ~~> y = \z -> if astEq z x then return y else mzero
 
--- | Exp instance for Mutable
--- instance Mutable Exp_ where
---   (==>) = (E .) . (,)
+{- | Exp instance for Mutable
+instance Mutable Exp_ where
+  (==>) = (E .) . (,)
+-}
 
 -- -- | Exp instance for Mutable
 -- instance Mutable Decl_ where
 --   (==>) = (D .) . (,)
 
--- | GuardedRhs instance for Mutable
--- instance Mutable GuardedRhs_ where
---   (==>) = (G .) . (,)
+{- | GuardedRhs instance for Mutable
+instance Mutable GuardedRhs_ where
+  (==>) = (G .) . (,)
+-}
--- src/Test/Mendel/MutationVariant.hs
+++ src/Test/Mendel/MutationVariant.hs
@@ -7,16 +7,18 @@ to inject faults into Haskell modules.
 -}
 module Test.Mendel.MutationVariant where
 
--- | A mutation operator which describes a semantic change that should be applied to
--- a Haskell module.
-data MuVariant = MutatePatternMatch
-               | MutateValues
-               | MutateFunctions
-               | MutateNegateIfElse
-               | MutateNegateGuards
-               | MutateOther String
-               | ReverseString
-               | ReverseClausesInPatternMatch
-               | SwapPlusMinus
-               | SwapIfElse
-  deriving (Show, Eq)
+{- | A mutation operator which describes a semantic change that should be applied to
+a Haskell module.
+-}
+data MuVariant
+    = MutatePatternMatch
+    | MutateValues
+    | MutateFunctions
+    | MutateNegateIfElse
+    | MutateNegateGuards
+    | MutateOther String
+    | ReverseString
+    | ReverseClausesInPatternMatch
+    | SwapPlusMinus
+    | SwapIfElse
+    deriving (Show, Eq)
